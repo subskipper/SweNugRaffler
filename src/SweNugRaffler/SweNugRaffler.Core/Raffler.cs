46725c46d0d9729266103eb0e773c19448c4c660
Henric <henric.edwards@telia.com>
2013-11-23 21:11:07 +0100

    More unit tests and added behaviour to randomly draw winner.

--- src/SweNugRaffler/SweNugRaffler.Core/Raffler.cs
+++ src/SweNugRaffler/SweNugRaffler.Core/Raffler.cs
@@ -20,5 +20,29 @@ namespace SweNugRaffler.Core
         {
             _entrants = new List<string>();
         }
+
+        public void AddEntrant(string entrant)
+        {
+            _entrants.Add(entrant);
+        }
+
+        public int NumberOfEntrants()
+        {
+            return _entrants.Count;
+        }
+
+        public string GetRandomWinner()
+        {
+            return GetWinner();
+        }
+
+        private string GetWinner()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+
+            var winnerIndex = random.Next(0, _entrants.Count - 1);
+
+            return _entrants[winnerIndex];
+        }
     }
 }
--- src/SweNugRaffler/SweNugRaffler.Tests/RafflerTests.cs
+++ src/SweNugRaffler/SweNugRaffler.Tests/RafflerTests.cs
@@ -18,5 +18,38 @@ namespace SweNugRaffler.Tests
 
             Assert.IsNotNull(raffler.Entrants);
         }
+
+        [Test]
+        public void Can_add_one_entrant()
+        {
+            var raffler = new Raffler();
+            raffler.AddEntrant("Per Persson");
+
+            Assert.AreEqual(1, raffler.NumberOfEntrants());
+        }
+
+        [Test]
+        public void Can_return_a_winner()
+        {
+            var raffler = new Raffler();
+
+            raffler.AddEntrant("Johan Johansson");
+
+            raffler.AddEntrant("Per Persson");
+
+            string winner = raffler.GetRandomWinner();
+
+            Assert.IsNotNullOrEmpty(winner);;
+        }
+
+        [Test]
+        public void Returns_specific_winner_when_only_one_entrant()
+        {
+            var raffler = new Raffler();
+            var expected = "Per Persson";
+            raffler.AddEntrant(expected);
+
+            Assert.AreEqual(expected, raffler.GetRandomWinner());
+        }
     }
 }
