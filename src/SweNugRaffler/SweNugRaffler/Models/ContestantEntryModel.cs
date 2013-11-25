7547216aae8fffca1741f376182c34b9ede5da09
Henric <henric.edwards@telia.com>
2013-11-25 23:41:29 +0100

    Token commit of silly model.

---
+++ src/SweNugRaffler/SweNugRaffler/Models/ContestantEntryModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SweNugRaffler.Models
+{
+    public class ContestantEntryModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+    }
+}
