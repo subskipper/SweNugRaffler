b8a6eebe9afcbea4abbc7af0ec1cd93efa7499a9
Henric <henric.edwards@telia.com>
2013-11-26 19:42:18 +0100

    Some very simple model binding and essentially no styling yet.

--- src/SweNugRaffler/SweNugRaffler/Controllers/HomeController.cs
+++ src/SweNugRaffler/SweNugRaffler/Controllers/HomeController.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using SweNugRaffler.Models;
 
 namespace SweNugRaffler.Controllers
 {
     public class HomeController : Controller
     {
+        //ToDO: Just a temp solution till db is up
+        private readonly List<ContestantEntryModel> _contestantList = new List<ContestantEntryModel>(); 
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +29,14 @@ namespace SweNugRaffler.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(ContestantEntryModel entry)
+        {
+            _contestantList.Add(entry);
+
+            return View(entry);
         }
     }
 }
--- src/SweNugRaffler/SweNugRaffler/Models/ContestantEntryModel.cs
+++ src/SweNugRaffler/SweNugRaffler/Models/ContestantEntryModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,11 @@ namespace SweNugRaffler.Models
 {
     public class ContestantEntryModel
     {
+        [DisplayName("Full Name:")]
         public string FullName { get; set; }
+        [DisplayName("E-Mail (no spam, promise!):")]
         public string Email { get; set; }
+        [DisplayName("Company (optional):")]
         public string Company { get; set; }
     }
 }
